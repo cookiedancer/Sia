fb5bc68ac319c5cf16c28bb2420eb74cc79d0d26
lindajstrauss <linda.j.burbano@gmail.com>
2016-03-23 18:59:21 -0400

    test while loop grade.cpp
    class assignment

---
+++ test while loop grade.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+
+using namespace std;
+
+int main ()
+
+
+{
+char letter;
+
+
+
+cout << "enter grade" << endl;
+ cin >> letter;
+
+
+
+while (letter == 'A')
+{ cout << "Great job" << endl;
+break;}
+
+while (letter == 'B')
+ {cout << "ok" << endl;
+break;}
+
+while (letter == 'C')
+ cout << "no" << endl;
+
+
+
+return 0;
+
+}
+
+
+
+
+
+
+
+
+
+
